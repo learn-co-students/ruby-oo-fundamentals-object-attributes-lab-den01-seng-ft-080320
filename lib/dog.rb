93133ce24c376339e15ff19c322331e3b6a171f9
sanjeevyg <yogi.sjv@gmail.com>
2020-08-08 16:32:54 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+    def name 
+        @name 
+    end
+    
+    def breed 
+        @breed 
+    end
+
+    def breed= breed
+        @breed = breed 
+    end
+
+    def name= dog_name 
+        @name = dog_name 
+    end
+end
